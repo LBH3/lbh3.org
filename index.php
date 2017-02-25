665f5548794b7cb88400023ba2e9dd129547a57b
Chasen Le Hara <chasen@chasenlehara.com>
2017-02-25 00:10:08 -0800

    Improve the home page design
    - Put a separator between the intro paragraph and the run title
    - Add some bottom margin to the run title and map embed
    - Make the run title slightly smaller
    - Put the run info into columns at larger widths

--- index.php
+++ index.php
@@ -21,29 +21,31 @@
         </small>
       </p>
 
+			<hr>
+
       <h2>Run #1795 — Passing Wind &amp; Hard in the Saddle</h2>
-      <dl>
-        <dt>Date:</dt>
-        <dd>Sunday 3/5 ~ 10:00 a.m.</dd>
-        <dt>Start:</dt>
-        <dd>32201 Forrestal Dr, Rancho Palos Verdes, CA 90275</dd>
-        <dt>Hares:</dt>
-        <dd>Passing Wind &amp; Hard in the Saddle</dd>
-        <dt>Donation:</dt>
-        <dd>$5 for run</dd>
+      <dl class="row">
+        <dt class="col-sm-3 text-sm-right">Date:</dt>
+        <dd class="col-sm-9">Sunday 3/5 ~ 10:00 a.m.</dd>
+        <dt class="col-sm-3 text-sm-right">Start:</dt>
+        <dd class="col-sm-9">32201 Forrestal Dr, Rancho Palos Verdes, CA 90275</dd>
+        <dt class="col-sm-3 text-sm-right">Hares:</dt>
+        <dd class="col-sm-9">Passing Wind &amp; Hard in the Saddle</dd>
+        <dt class="col-sm-3 text-sm-right">Donation:</dt>
+        <dd class="col-sm-9">$5 for run</dd>
       </dl>
 
 			<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3317.896577447866!2d-118.34984288479319!3d33.7374903806927!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x80dd497597d10bf7%3A0xd57ee00dc4fab75c!2s32201+Forrestal+Dr%2C+Rancho+Palos+Verdes%2C+CA+90275!5e0!3m2!1sen!2sus!4v1488005074140" width="100%" height="368" frameborder="0" style="border:0" allowfullscreen></iframe>
 
-      <dl>
-        <dt>Note from the hares:</dt>
-        <dd>
+      <dl class="row">
+        <dt class="col-md-3 text-md-right">From the hares:</dt>
+        <dd class="col-md-9">
           <p>
 						These two old goats are at it again they have a great trail with shiggy and views for the pack to go along with cold golden nectar.
           </p>
         </dd>
-        <dt>D'Erections:</dt>
-        <dd>
+        <dt class="col-md-3 text-md-right">D'Erections:</dt>
+        <dd class="col-md-9">
           <p>
 						From Long Beach — get on I-405 N and follow I-405 N to the  I-110 S. Take the exit toward Gaffey St/San Pedro. Turn left onto N Gaffey St then Turn right onto W 1st St proceed for about a mile and turn left onto S Western Ave. Then turn right onto W 25th St continue until it turns into Palos Verdes Dr S then turn right onto Forrestal Dr to reach your destination.
           </p>
