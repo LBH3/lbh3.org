4ee19c13a62dffa17bc27f585fd78aa4f998cc1c
Chasen Le Hara <chasen@chasenlehara.com>
2017-03-13 21:45:08 -0700

    Update after 2017-03-12 hash

--- index.php
+++ index.php
@@ -23,28 +23,31 @@
 
 			<hr>
 
-      <h2>Run #1795</h2>
+      <h2>Run #1796</h2>
       <dl class="row">
         <dt class="col-sm-3 text-sm-right">Date:</dt>
-        <dd class="col-sm-9">Sunday 3/12 ~ 10:00 a.m.</dd>
+        <dd class="col-sm-9"><strong>Thursday</strong> 3/16 ~ 6:30 p.m.</dd>
         <dt class="col-sm-3 text-sm-right">Start:</dt>
-        <dd class="col-sm-9">2601 Blandford Dr, Rowland Heights, CA 91748</dd>
+        <dd class="col-sm-9">11155 183rd St, Cerritos, CA 90703</dd>
         <dt class="col-sm-3 text-sm-right">Hares:</dt>
-        <dd class="col-sm-9">Mr. Rat’s Ass &amp; amigos</dd>
+        <dd class="col-sm-9">Kowpaddy Kowboy &amp; Spitty Spitty Gang Bang</dd>
         <dt class="col-sm-3 text-sm-right">Donation:</dt>
         <dd class="col-sm-9">$5 for run</dd>
       </dl>
 
-			<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3308.772945956592!2d-117.89987628478703!3d33.97267498062765!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x80c32aeba5ca20db%3A0x1ab7194cbe63cfe3!2s2601+Blandford+Dr%2C+Rowland+Heights%2C+CA+91748!5e0!3m2!1sen!2sus!4v1488869668772" width="100%" height="368" frameborder="0" style="border:0" allowfullscreen></iframe>
+			<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3312.8610962105263!2d-118.09828478478974!3d33.86747048065664!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x80dd2d71c6ee5375%3A0x1655f200f2611ea3!2s11155+183rd+St%2C+Cerritos%2C+CA+90703!5e0!3m2!1sen!2sus!4v1489466463859" width="100%" height="368" frameborder="0" style="border:0" allowfullscreen></iframe>
 
       <dl class="row">
         <dt class="col-md-3 text-md-right">From the hares:</dt>
         <dd class="col-md-9">
 					<p>
-						NOTICE: Daylight Saving Time begins!
-					</p>
-          <p>
-						Okay, so MRA signed up to hare the last Sunday run, but as a condition of his being on hare probation, all 3 trailmasters (Victoria Secretions, KooKoo and Hokey Tokey) will be escorting him on this trail to guide him around any poison oak.  So while the trail might not be Run of the Year like Victoria and KooKoo's was last year, at least you won't be needing any Tecnu!  Small dog friendly, not stroller friendly.  There are several people who can only do Sunday runs, so after this hash, we start Thursday runs and you won't see them again until October, so come out and say goodbye!
+						The first Thursday run of the year will be in Cerritos. Come on out before you leave for Hash Springs and pre-lube at the St. KowPaddy night run with Spitty Spitty Gang Bang as the kohare! A couple beer checks and some urban shiggy. It should be fun.
+          </p>
+        </dd>
+        <dt class="col-md-3 text-md-right">Directions:</dt>
+        <dd class="col-md-9">
+					<p>
+						Very simple. From wherever you are, go to the Cerritos Mall. At 183rd and Gridley, drive to the back of the Guitar Center area. LFH.
           </p>
         </dd>
       </dl>
