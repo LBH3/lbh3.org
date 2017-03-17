4a09118c43c441c2b462a103c0168b2d14084ee9
Chasen Le Hara <chasen@chasenlehara.com>
2017-03-17 15:47:52 -0700

    Add 2017-03-23 run details to the home page

--- index.php
+++ index.php
@@ -23,31 +23,32 @@
 
 			<hr>
 
-      <h2>Run #1797</h2>
+      <h2>Run #1798</h2>
+
       <dl class="row">
         <dt class="col-sm-3 text-sm-right">Date:</dt>
-        <dd class="col-sm-9"><strong>Thursday</strong> 3/16 ~ 6:30 p.m.</dd>
+        <dd class="col-sm-9">Thursday 3/23 ~ 6:30 p.m.</dd>
         <dt class="col-sm-3 text-sm-right">Start:</dt>
-        <dd class="col-sm-9">11155 183rd St, Cerritos, CA 90703</dd>
+        <dd class="col-sm-9">Belmont Pier Lot, Long Beach, CA 90803</dd>
         <dt class="col-sm-3 text-sm-right">Hares:</dt>
-        <dd class="col-sm-9">Kowpaddy Kowboy &amp; Spitty Spitty Gang Bang</dd>
+        <dd class="col-sm-9">Sad Bastard</dd>
         <dt class="col-sm-3 text-sm-right">Donation:</dt>
         <dd class="col-sm-9">$5 for run</dd>
       </dl>
 
-			<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3312.8610962105263!2d-118.09828478478974!3d33.86747048065664!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x80dd2d71c6ee5375%3A0x1655f200f2611ea3!2s11155+183rd+St%2C+Cerritos%2C+CA+90703!5e0!3m2!1sen!2sus!4v1489466463859" width="100%" height="368" frameborder="0" style="border:0" allowfullscreen></iframe>
+			<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3317.076805523463!2d-118.14922138479272!3d33.75868108068696!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x80dd30397165c6b7%3A0x463f8970846113b5!2sBelmont+Pier+Lot!5e0!3m2!1sen!2sus!4v1489790727488" width="100%" height="368" frameborder="0" style="border:0" allowfullscreen></iframe>
 
       <dl class="row">
         <dt class="col-md-3 text-md-right">From the hares:</dt>
         <dd class="col-md-9">
 					<p>
-						The first Thursday run of the year will be in Cerritos. Come on out before you leave for Hash Springs and pre-lube at the St. KowPaddy night run with Spitty Spitty Gang Bang as the kohare! A couple beer checks and some urban shiggy. It should be fun.
+						Parking is free after 6pm at the Belmont Pier Lot.
           </p>
         </dd>
         <dt class="col-md-3 text-md-right">Directions:</dt>
         <dd class="col-md-9">
 					<p>
-						Very simple. From wherever you are, go to the Cerritos Mall. At 183rd and Gridley, drive to the back of the Guitar Center area. LFH.
+						From CSULB area take 7th St. to Ximeno Ave and turn left. Continue until you reach Livingston Dr and turn right then make a left onto Termino continue until it ends and LFH in the parking lot.
           </p>
         </dd>
       </dl>
