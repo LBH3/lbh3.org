e3cfd05fcf675403f60345f66d39675c5c77ddd1
Chasen Le Hara <chasen@chasenlehara.com>
2017-03-26 16:56:16 -0700

    Update for the 2017-03-30 trail

--- index.php
+++ index.php
@@ -23,33 +23,32 @@
 
 			<hr>
 
-      <h2>Run #1798</h2>
+      <h2>Run #1799</h2>
 
       <dl class="row">
         <dt class="col-sm-3 text-sm-right">Date:</dt>
-        <dd class="col-sm-9">Thursday 3/23 ~ 6:30 p.m.</dd>
+        <dd class="col-sm-9">Thursday 3/30 ~ 6:30 p.m.</dd>
         <dt class="col-sm-3 text-sm-right">Start:</dt>
-        <dd class="col-sm-9">4320 E Olympic Plaza, Long Beach, CA 90803 </dd>
+        <dd class="col-sm-9">1302 Parkview Ave, Manhattan Beach, CA 90266</dd>
         <dt class="col-sm-3 text-sm-right">Hares:</dt>
-        <dd class="col-sm-9">Sad Bastard</dd>
+        <dd class="col-sm-9">Victoria’s Secretion</dd>
         <dt class="col-sm-3 text-sm-right">Donation:</dt>
         <dd class="col-sm-9">$5 for run</dd>
       </dl>
 
-			<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3317.123878659738!2d-118.14619228479268!3d33.75746458068706!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x80dd304ef15e5b63%3A0x247184c974767417!2s4320+E+Olympic+Plaza%2C+Long+Beach%2C+CA+90803!5e0!3m2!1sen!2sus!4v1490146701116" width="100%" height="368" frameborder="0" style="border:0" allowfullscreen></iframe>
+			<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3311.64326266019!2d-118.39450328478894!3d33.89884028064783!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x80c2b407f29b04f5%3A0x6089205f9b38456c!2s1302+Parkview+Ave%2C+Manhattan+Beach%2C+CA+90266!5e0!3m2!1sen!2sus!4v1490572387597" width="100%" height="368" frameborder="0" style="border:0" allowfullscreen></iframe>
 
       <dl class="row">
-				<!--
-        <dt class="col-md-3 text-md-right">From the hares:</dt>
-        <dd class="col-md-9">
-					<p>
-          </p>
-        </dd>
-			-->
         <dt class="col-md-3 text-md-right">Directions:</dt>
         <dd class="col-md-9">
 					<p>
-						From CSULB area take 7th St. to Park Ave and turn left. Continue until you reach Ocean Blvd and turn right. Make a left at the next stop sign (Bennett), and another left into the Belmont Pool parking lot.  LFH.
+						Take the 405 North to Rosecrans Ave. Turn left at the exit and head West. Proceed about a mile down Rosecrans and then turn left on Park Way and then right on Parkview Ave. After about a .2 miles turn left on Park Pl. and then go to the North West corner of the parking lot and LFH
+          </p>
+        </dd>
+        <dt class="col-md-3 text-md-right">From the hares:</dt>
+        <dd class="col-md-9">
+					<p>
+						LBH3 needs hares for thursday night trails. Trail must be West of Beach and South of the Manchester/Rosecrans border. If you want to hare but need help let your trailmasters know and they will get someone to help.
           </p>
         </dd>
       </dl>
