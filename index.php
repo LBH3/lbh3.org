9c395ddd7acd3c5dc7cb6725fd9c1e6bb271ab74
Chasen Le Hara <chasen@chasenlehara.com>
2017-04-16 21:47:33 -0700

    Update after 1800 run

--- index.php
+++ index.php
@@ -23,45 +23,50 @@
 
 			<hr>
 
-			<p class="text-center">
-				<a href="events/jesus-cuervo-1800-trail/">
-					Register for our Jesus Cuervo 1800 trail!
-				</a>
-			</p>
-
-			<hr>
-
-      <h2>Run #1802</h2>
+      <h2 style="color: green">
+				Safety Committee Trail
+				<br>
+				<small class="text-muted">Run #1804</small>
+			</h2>
 
       <dl class="row">
         <dt class="col-sm-3 text-sm-right">Date:</dt>
-        <dd class="col-sm-9">Thursday 4/13 ~ 6:30 p.m.</dd>
+        <dd class="col-sm-9">Thursday 4/20 ~ 6:30 p.m.</dd>
         <dt class="col-sm-3 text-sm-right">Start:</dt>
-        <dd class="col-sm-9">611 Federation Dr, Long Beach, CA 90814</dd>
+        <dd class="col-sm-9">2910 E 55th Way, Long Beach, CA 90805</dd>
         <dt class="col-sm-3 text-sm-right">Hares:</dt>
-        <dd class="col-sm-9">Pillsbury Blow Boy, Snowblower, &amp; Hit Her in the Shitter</dd>
+        <dd class="col-sm-9">Safety Committee</dd>
+        <dt class="col-sm-3 text-sm-right">Bring:</dt>
+        <dd class="col-sm-9">Money, I.D, Lights, and <span style="color: green">Green Shiggy Socks</span></dd>
         <dt class="col-sm-3 text-sm-right">Donation:</dt>
         <dd class="col-sm-9">$5 for run</dd>
       </dl>
 
-			<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3316.4548973824635!2d-118.13689038479231!3d33.77474928068228!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x80dd31c830e4df91%3A0x593b248390fb98ba!2s611+Federation+Dr%2C+Long+Beach%2C+CA+90814!5e0!3m2!1sen!2sus!4v1491543354317" width="100%" height="368" frameborder="0" style="border:0" allowfullscreen></iframe>
+			<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3313.274808027482!2d-118.15889168479019!3d33.85680798065966!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x80dd32e24fd54a0d%3A0x3ee8fcc33a2a14b2!2s2910+E+55th+Way%2C+Long+Beach%2C+CA+90805!5e0!3m2!1sen!2sus!4v1492404189707" width="100%" height="368" frameborder="0" style="border:0" allowfullscreen></iframe>
 
-			<!--
       <dl class="row">
         <dt class="col-md-3 text-md-right">Directions:</dt>
         <dd class="col-md-9">
 					<p>
-						Take the 405 North to Rosecrans Ave. Turn left at the exit and head West. Proceed about a mile down Rosecrans and then turn left on Park Way and then right on Parkview Ave. After about a .2 miles turn left on Park Pl. and then go to the North West corner of the parking lot and LFH
+						The start is at the end of "E 55th Way"
           </p>
         </dd>
         <dt class="col-md-3 text-md-right">From the hares:</dt>
         <dd class="col-md-9">
 					<p>
-						Ben Dover and Family will be haring inside the box for the first time in years
+						Ok Hashers and Harriets. 420 lands on a Thursday and The Safety Committee is keeping the pack safe and high as we go hash in North Long Beach. So cum and Joint The Safety Committee as we take you on some fresh green trail.
+					</p>
+					<p style="color: green">
+						MUST WEAR GREEN!!!
+          </p>
+        </dd>
+        <dt class="col-md-3 text-md-right">On-on:</dt>
+        <dd class="col-md-9">
+					<p>
+						Coopers House Of Brew Masters. 4823 Paramount Blvd, Lakewood, CA 90712
           </p>
         </dd>
       </dl>
-		-->
     </main>
     <footer>
       <p class="container text-muted">
