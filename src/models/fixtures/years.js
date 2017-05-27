39a10b0c1cc24af0bc6453f148659617157edc79
Chasen Le Hara <chasen@chasenlehara.com>
2017-05-26 19:33:01 -0700

    Add the year model

---
+++ src/models/fixtures/years.js
@@ -0,0 +1,18 @@
+import fixture from 'can-fixture';
+import Year from '../year';
+
+const store = fixture.store([{
+  id: 2013
+}, {
+  id: 2014
+}, {
+  id: 2015
+}, {
+  id: 2016
+}, {
+  id: 2017
+}], Year.connection.algebra);
+
+fixture('/years/{id}', store);
+
+export default store;
--- src/models/test.js
+++ src/models/test.js
@@ -1,3 +1,4 @@
 import './fixtures/';
 
 import 'lbh3/models/position-test';
+import 'lbh3/models/year-test';
