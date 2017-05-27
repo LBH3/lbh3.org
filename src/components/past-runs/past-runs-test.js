c806bb75ef1a8133eeced8b0acc77a6d32d46e92
Chasen Le Hara <chasen@chasenlehara.com>
2017-05-26 20:31:30 -0700

    Show the static year pages in Past Runs

--- src/components/past-runs/past-runs-test.js
+++ src/components/past-runs/past-runs-test.js
@@ -4,7 +4,7 @@ import { ViewModel } from './past-runs';
 // ViewModel unit tests
 QUnit.module('lbh3/components/past-runs');
 
-QUnit.test('Has message', function(){
+QUnit.test('Has positionsPromise', function(){
   var vm = new ViewModel();
-  QUnit.equal(vm.message, 'This is the lbh3-past-runs component');
+  QUnit.ok(vm.positionsPromise, 'okay');
 });
