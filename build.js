6ad396279ff117fbc9bcee0624089225fcaf0d4d
Chasen Le Hara <chasen@chasenlehara.com>
2017-05-28 16:43:19 -0700

    Bundle images in dist

--- build.js
+++ build.js
@@ -3,5 +3,8 @@ var stealTools = require("steal-tools");
 var buildPromise = stealTools.build({
   config: __dirname + "/package.json!npm"
 }, {
-  bundleAssets: true
+  bundleAssets: {
+    glob: 'images/*',
+    infer: false
+  }
 });
