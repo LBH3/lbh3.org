554ccb65d57f457589b0497349a90a7e333e1eaf
Chasen Le Hara <chasen@chasenlehara.com>
2017-05-28 22:57:28 -0700

    Prevent bad Ajax requests from being made from the past-run component

--- src/components/past-run/past-run-test.js
+++ src/components/past-run/past-run-test.js
@@ -6,5 +6,9 @@ QUnit.module('lbh3/components/past-run');
 
 QUnit.test('Has templatePromise', function(){
   var vm = new ViewModel();
+  vm.day = '25';
+  vm.month = '05';
+  vm.trailNumber = 1809;
+  vm.year = 2017;
   QUnit.ok(vm.templatePromise, 'okay');
 });
