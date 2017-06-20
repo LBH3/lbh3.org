ebf842e9d76b1a45f46bdb8800e226cdc5737b7d
Chasen Le Hara <chasen@chasenlehara.com>
2017-06-20 03:06:27 -0700

    Use the new event model on the “Add a new trail” page

--- src/components/hareline/add/add-test.js
+++ src/components/hareline/add/add-test.js
@@ -4,13 +4,15 @@ import { ViewModel } from './add';
 // ViewModel unit tests
 QUnit.module('lbh3/components/hareline/add');
 
-QUnit.test('createTrail resets the form', function(){
+QUnit.test('createTrail resets the form', function(done) {
   var vm = new ViewModel({
     runNumber: 1800,
     startTime: '10:00 a.m.',
     trailDate: '4/15'
   });
   QUnit.equal(vm.runNumber, 1800);
-  vm.createTrail();
-  QUnit.equal(vm.runNumber, 0);
+  vm.createTrail().then(function() {
+    QUnit.equal(vm.runNumber, 0);
+    done();
+  });
 });
--- src/components/hareline/add/add.js
+++ src/components/hareline/add/add.js
@@ -1,17 +1,34 @@
 import Component from 'can-component';
 import DefineMap from 'can-define/map/';
+import Event from '~/models/event';
 import './add.less';
 import view from './add.stache';
 
 export const ViewModel = DefineMap.extend({
+  createdTrail: Event,
+  creatingTrailPromise: {
+    set: function(creatingTrailPromise) {
+      return creatingTrailPromise.then((createdTrail) => {
+        this.createdTrail = createdTrail;
+        this.runNumber = createdTrail.runNumber + 1;
+        this.startTime = '';
+        this.trailDate = '';
+      }, (error) => {
+        console.error('Failed to create trail with error:', error);
+      });
+    }
+  },
   runNumber: 'number',
   startTime: 'string',
   trailDate: 'string',
 
   createTrail: function() {
-    this.runNumber = '';
-    this.startTime = '';
-    this.trailDate = '';
+    const newTrailData = {
+      runNumber: this.runNumber,
+      startTime: this.startTime,
+      trailDate: this.trailDate,
+    };
+    return this.creatingTrailPromise = new Event(newTrailData).save();
   }
 });
 
