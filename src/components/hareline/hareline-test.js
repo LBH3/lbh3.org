96de07e1c1850e76ff188870694088445b36783f
Chasen Le Hara <chasen@chasenlehara.com>
2017-07-01 11:02:17 -0700

    Move hareline/current.html file inline to the component

--- src/components/hareline/hareline-test.js
+++ src/components/hareline/hareline-test.js
@@ -3,8 +3,3 @@ import { ViewModel } from './hareline';
 
 // ViewModel unit tests
 QUnit.module('lbh3/components/hareline');
-
-QUnit.test('Has currentHareline', function(){
-  var vm = new ViewModel();
-  QUnit.ok(vm.currentHareline, 'okay');
-});
