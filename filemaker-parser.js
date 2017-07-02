862e162ff0eb7b4841c7b0124d8acf88b1a7e312
Chasen Le Hara <chasen@chasenlehara.com>
2017-07-01 23:51:08 -0700

    Add support for parsing the member list from FileMaker

--- filemaker-parser.js
+++ filemaker-parser.js
@@ -1,11 +1,59 @@
 const fs = require('fs');
 const xml2js = require('xml2js');
 
-const parser = new xml2js.Parser();
+fs.readFile(`${__dirname}/filemaker/list-of-members.xml`, function(readError, data) {
+  if (readError) {
+    console.error('Error while reading list-of-members.xml:', readError);
+  } else {
+    const parser = new xml2js.Parser();
+    parser.parseString(data, function(parseError, result) {
+      if (parseError) {
+        console.error('Error while parsing list-of-members.xml:', parseError);
+      } else {
+        const json = JSON.stringify(result);
+        fs.writeFile(`${__dirname}/filemaker/list-of-members.json`, json, function(writeError, data) {
+          if (parseError) {
+            console.error('Error while writing list-of-members.json:', writeError);
+          } else {
+            console.info('Successfully wrote list-of-members.json');
+          }
+        });
+
+        const data = result.FMPXMLRESULT;
+        const metadata = data.METADATA;
+        let fields;
+        const hashers = [];
+        metadata.forEach(function(parsedFields) {
+          fields = parsedFields.FIELD.map(function(field) {
+            return field.$.NAME;
+          });
+        });
+        console.info('Fields in list-of-members.xml:', fields);
+        data.RESULTSET.forEach(function(rows) {
+          rows.ROW.forEach(function(row) {
+            const hasher = {};
+            row.COL.forEach(function(column, i) {
+              const filteredData = column.DATA.filter(function(data) {
+                return data;
+              });
+              hasher[fields[i]] = (filteredData.length > 1) ? filteredData.join('; ') : filteredData[0];
+            });
+            hasher.RECORDID = row.$.RECORDID;
+            console.info('Hasher:', hasher);
+            hashers.push(hasher);
+          });
+        });
+        console.info('Total number of hashers:', hashers.length);
+      }
+    });
+  }
+});
+
 fs.readFile(`${__dirname}/filemaker/run-list-all.xml`, function(readError, data) {
   if (readError) {
     console.error('Error while reading the XML file:', readError);
   } else {
+    const parser = new xml2js.Parser();
     parser.parseString(data, function(parseError, result) {
       if (parseError) {
         console.error('Error while parsing the XML file:', parseError);
