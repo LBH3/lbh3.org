eae0a4da1501dbe195ca7945be877f223b2d4ed9
Chasen Le Hara <chasen@chasenlehara.com>
2017-07-06 22:35:35 -0700

    Implement api/events
    Closes https://github.com/LBH3/lbh3.org/issues/26

--- src/models/events.model.js
+++ src/models/events.model.js
@@ -1,15 +1,12 @@
 // See http://docs.sequelizejs.com/en/latest/docs/models-definition/
 // for more of what you can do here.
 const Sequelize = require('sequelize');
+const tables = require('../../config/tables');
 
 module.exports = function (app) {
   const sequelizeClient = app.get('sequelizeClient');
-  const events = sequelizeClient.define('events', {
-    trailNumber: {
-      type: Sequelize.INTEGER,
-      allowNull: false
-    }
-  }, {
+  const tableColumns = tables.getDefinitionsWithSequelize(Sequelize);
+  const events = sequelizeClient.define('events', tableColumns.events, {
     hooks: {
       beforeCount(options) {
         options.raw = true;
