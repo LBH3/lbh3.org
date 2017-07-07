1b1f496d1120c00da5ecc395f5f56e1e6f47e8f1
Chasen Le Hara <chasen@chasenlehara.com>
2017-07-06 22:35:35 -0700

    Implement api/events
    Closes https://github.com/LBH3/lbh3.org/issues/26

--- src/models/users.model.js
+++ src/models/users.model.js
@@ -1,15 +1,12 @@
 // See http://docs.sequelizejs.com/en/latest/docs/models-definition/
 // for more of what you can do here.
 const Sequelize = require('sequelize');
+const tables = require('../../config/tables');
 
 module.exports = function (app) {
   const sequelizeClient = app.get('sequelizeClient');
-  const users = sequelizeClient.define('users', {
-  
-  
-    facebookId: { type: Sequelize.STRING },
-  
-  }, {
+  const tableColumns = tables.getDefinitionsWithSequelize(Sequelize);
+  const users = sequelizeClient.define('users', tableColumns.tables, {
     hooks: {
       beforeCount(options) {
         options.raw = true;
