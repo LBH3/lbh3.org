80891298efcb1c80b9e1ee3dcddb555063e05e32
Chasen Le Hara <chasen@chasenlehara.com>
2017-07-10 22:34:07 -0700

    Allow runs to be created
    Closes https://github.com/LBH3/lbh3.org/issues/30

--- public/components/hareline/add/add.js
+++ public/components/hareline/add/add.js
@@ -2,7 +2,14 @@ import Component from 'can-component';
 import DefineMap from 'can-define/map/';
 import Event from '~/models/event';
 import './add.less';
+import moment from 'moment';
 import view from './add.stache';
+
+const oneWeekFromDate = function(date) {
+  const dateAsMoment = moment(date);
+  dateAsMoment.add(7, 'days');
+  return dateAsMoment.format().substr(0, 10);
+};
 
 export const ViewModel = DefineMap.extend({
   createdTrail: Event,
@@ -10,22 +17,74 @@ export const ViewModel = DefineMap.extend({
     set: function(creatingTrailPromise) {
       return creatingTrailPromise.then((createdTrail) => {
         this.createdTrail = createdTrail;
-        this.startTime = '';
-        this.trailDate = '';
+        this.startDate = oneWeekFromDate(createdTrail.startDateAsMoment);
         this.trailNumber = createdTrail.trailNumber + 1;
       }, (error) => {
         console.error('Failed to create trail with error:', error);
       });
     }
   },
-  startTime: 'string',
-  trailDate: 'string',
-  trailNumber: 'number',
+  lastTrail: {
+    get: function(lastValue, setValue) {
+      if (lastValue) {
+        return lastValue;
+      }
+      this.lastTrailPromise.then(events => {
+        setValue(events[0]);
+      });
+    }
+  },
+  lastTrailPromise: {
+    get: function() {
+      return Event.connection.getList({
+        $limit: 1,
+        $sort: {
+          startDatetime: -1
+        }
+      });
+    }
+  },
+  startDate: {
+    type: 'string',
+    get: function(lastValue) {
+      if (lastValue) {
+        return lastValue;
+      }
+      const lastTrail = this.lastTrail;
+      if (lastTrail && lastTrail.startDateAsMoment) {
+        return oneWeekFromDate(lastTrail.startDateAsMoment);
+      }
+    }
+  },
+  startTime: {
+    type: 'string',
+    get: function(lastValue) {
+      if (lastValue) {
+        return lastValue;
+      }
+      const lastTrail = this.lastTrail;
+      if (lastTrail && lastTrail.startDatetime) {
+        return lastTrail.startDateAsMoment.format().substr(11, 8);
+      }
+    }
+  },
+  trailNumber: {
+    type: 'number',
+    get: function(lastValue) {
+      if (lastValue) {
+        return lastValue;
+      }
+      const lastTrail = this.lastTrail;
+      if (lastTrail && lastTrail.trailNumber) {
+        return lastTrail.trailNumber + 1;
+      }
+    }
+  },
 
   createTrail: function() {
+    const startDatetime = moment(`${this.startDate} ${this.startTime}`).format();
     const newTrailData = {
-      startTime: this.startTime,
-      trailDate: this.trailDate,
+      startDatetime: startDatetime,
       trailNumber: this.trailNumber
     };
     return this.creatingTrailPromise = new Event(newTrailData).save();
