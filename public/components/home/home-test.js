69fb57997b0a43ca5c45a346a080fa698f3d933d
Chasen Le Hara <chasen@chasenlehara.com>
2017-07-17 22:32:55 -0700

    Generate the front page from the API
    Closes https://github.com/LBH3/lbh3.org/issues/20

--- public/components/home/home-test.js
+++ public/components/home/home-test.js
@@ -4,7 +4,7 @@ import { ViewModel } from './home';
 // ViewModel unit tests
 QUnit.module('lbh3/components/home');
 
-QUnit.test('Has currentTrail', function(){
+QUnit.test('Has googleMapsKey', function(){
   var vm = new ViewModel();
-  QUnit.ok(vm.currentTrail, 'okay');
+  QUnit.ok(vm.googleMapsKey, 'okay');
 });
