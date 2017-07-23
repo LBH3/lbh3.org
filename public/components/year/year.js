28e8405ee219f69c4defad48a360aaf0da56bbd9
Chasen Le Hara <chasen@chasenlehara.com>
2017-07-23 10:45:59 -0700

    Limit the Past Runs page to just what’s in the past

--- public/components/year/year.js
+++ public/components/year/year.js
@@ -8,15 +8,18 @@ export const ViewModel = DefineMap.extend({
   eventsByMonth: DefineList,
   eventsPromise: {
     get: function() {
-      const year = this.year;
+      const currentDate = new Date();
+      const currentYear = currentDate.getFullYear();
+      const endDate = (currentYear === this.year) ? currentDate : new Date(this.year + 1, 0, 0);
+      const startDate = new Date(this.year, 0, 0);
       return Event.connection.getList({
         $limit: 100,
         $sort: {
           startDatetime: 1
         },
         startDatetime: {
-          $gte: new Date(year, 0, 0),
-          $lte: new Date(year + 1, 0, 0)
+          $gte: startDate,
+          $lte: endDate
         }
       }).then((events) => {
         this.eventsByMonth = Event.groupByMonth(events);
