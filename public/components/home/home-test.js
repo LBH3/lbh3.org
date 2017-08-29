a90daad8c99767dd84f5f190eaff6338024b9cf0
Chasen Le Hara <chasen@chasenlehara.com>
2017-08-28 21:29:04 -0700

    Fix can-set warning on the home page

--- public/components/home/home-test.js
+++ public/components/home/home-test.js
@@ -3,8 +3,3 @@ import { ViewModel } from './home';
 
 // ViewModel unit tests
 QUnit.module('lbh3/components/home');
-
-QUnit.test('Has googleMapsKey', function(){
-  var vm = new ViewModel();
-  QUnit.ok(vm.googleMapsKey, 'okay');
-});
