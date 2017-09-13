097220001376f4ab2a0ff1c57e794042ade29012
Chasen Le Hara <chasen@chasenlehara.com>
2017-09-13 12:20:31 -0700

    Add more browsers to Sauce Labs testing and test the oldest supported versions

--- public/test.sauce-labs.js
+++ public/test.sauce-labs.js
@@ -4,24 +4,40 @@ const testSauceLabs = require('test-saucelabs');
 
 testSauceLabs({
   platforms: [{
-  	browserName: 'internet explorer',
-  	platform: 'Windows 10',
-  	version: '11.0'
+    'appium-version': '1.6.3',
+    browserName: 'Browser',
+    deviceName: 'Android Emulator',
+    platform: 'Android 4.4'
   }, {
-  	browserName: 'MicrosoftEdge',
-  	platform: 'Windows 10'
+    'appium-version': '1.6.3',
+    browserName: 'safari',
+    deviceName: 'iPhone 6 Simulator',
+    platform: 'iOS',
+    version: '9'
   }, {
-  	browserName: 'firefox',
-  	platform: 'Windows 10',
-  	version: 'latest'
+    browserName: 'internet explorer',
+    platform: 'Windows 7',
+    version: '11'
   }, {
-  	browserName: 'googlechrome',
-  	platform: 'Windows 10',
-  	version: 'latest'
+    browserName: 'MicrosoftEdge',
+    platform: 'Windows 10',
+    version: '14'
+  }, {
+    browserName: 'firefox',
+    platform: 'Windows 7',
+    version: '47'
+  }, {
+    browserName: 'googlechrome',
+    platform: 'Windows 7',
+    version: '46'
+  }, {
+    browserName: 'safari',
+    platform: 'OS X 10.11',
+    version: '9'
   }],
-	urls: [{
+  urls: [{
     name: 'lbh3',
     url: 'http://localhost:3000/test.html?hidepassed'
   }],
-	zeroAssertionsPass: false
+  zeroAssertionsPass: false
 });
