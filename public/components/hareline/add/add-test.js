3ba60fe5b65920788b5925be33141afdcbf0f0aa
Chasen Le Hara <chasen@chasenlehara.com>
2017-09-12 18:11:04 -0700

    Use QUnit’s async API correctly

--- public/components/hareline/add/add-test.js
+++ public/components/hareline/add/add-test.js
@@ -4,15 +4,16 @@ import { ViewModel } from './add';
 // ViewModel unit tests
 QUnit.module('lbh3/components/hareline/add');
 
-QUnit.test('createTrail resets the form', function(done) {
+QUnit.test('createTrail resets the form', function(assert) {
+  var done = assert.async();
   var vm = new ViewModel({
-    startTime: '10:00 a.m.',
-    trailDate: '4/15',
+    startDate: '2017-04-15',
+    startTime: '10:00:00',
     trailNumber: 1800
   });
-  QUnit.equal(vm.trailNumber, 1800);
+  assert.equal(vm.trailNumber, 1800);
   vm.createTrail().then(function() {
-    QUnit.equal(vm.trailNumber, 1801);
+    assert.equal(vm.trailNumber, 1801);
     done();
   });
 });
