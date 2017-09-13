75919ff4f4ebd8191d39be0d6a767dae8b49bb7e
Chasen Le Hara <chasen@chasenlehara.com>
2017-09-12 18:14:43 -0700

    Replace unfetch with jQuery
    unfetch will use the native fetch() API if it’s available, which creates inconsistencies between browsers.

--- public/models/feathers-client.js
+++ public/models/feathers-client.js
@@ -1,13 +1,13 @@
 import '~/polyfills';
 import auth from 'feathers-authentication-client';
 import feathers from 'feathers/client';
-import fetch from 'unfetch';
 import hooks from 'feathers-hooks';
+import jQuery from 'jquery';
 import loader from '@loader';
 import rest from 'feathers-rest/client';
 
 const feathersClient = feathers()
-  .configure(rest(loader.serviceBaseURL).fetch(fetch.bind(window)))
+  .configure(rest(loader.serviceBaseURL).jquery(jQuery))
   .configure(hooks())
   .configure(auth({
   }));
