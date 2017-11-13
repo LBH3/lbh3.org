4b3583207d36020223d056a611bdb5a88bb0dd0c
Chasen Le Hara <chasen@chasenlehara.com>
2017-11-12 21:06:24 -0800

    Use our custom @lbh3.org email addresses
    Closes https://github.com/LBH3/lbh3.org/issues/50

--- public/components/past-runs/past-runs.js
+++ public/components/past-runs/past-runs.js
@@ -1,6 +1,5 @@
 import Component from 'can-component';
 import DefineMap from 'can-define/map/';
-import Position from '~/models/position';
 import Year from '~/models/year';
 import route from 'can-route';
 import view from './past-runs.stache';
@@ -16,19 +15,6 @@ export const ViewModel = DefineMap.extend({
   },
   get latestSnoozeUrl () {
     return '';// TODO
-  },
-  positions: {
-    get: function(lastValue, setValue) {
-      if (lastValue) {
-        return lastValue;
-      }
-      this.positionsPromise.then(setValue);
-    }
-  },
-  positionsPromise: {
-    value: function() {
-      return Position.getList({});
-    }
   },
   routeForYear: function(year) {
     const routeParams = {page: 'events'};
@@ -59,10 +45,6 @@ export const ViewModel = DefineMap.extend({
         }
       });
     }
-  },
-  get webmaster() {
-    const positions = this.positions;
-    return (positions) ? positions[positions.length - 1]['people'][0] : null;
   }
 });
 
