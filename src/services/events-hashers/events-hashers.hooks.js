d913c2468681c4baf4d2236feb4836cd7c7ebb48
Chasen Le Hara <chasen@chasenlehara.com>
2017-11-18 11:32:49 -0800

    Update counts/totals when adding/removing hashers from runs
    Closes https://github.com/LBH3/lbh3.org/issues/79

--- src/services/events-hashers/events-hashers.hooks.js
+++ src/services/events-hashers/events-hashers.hooks.js
@@ -1,10 +1,50 @@
 const { authenticate } = require('feathers-authentication').hooks;
+const { Client } = require('pg');
 const authHook = require('../../hooks/auth');
+
+const getFirstOrLastTrailForHasher = function(first, hasherId) {
+  const sortDirection = first ? 'ASC' : 'DESC';
+  return new Promise(function(resolve, reject) {
+    const client = new Client({
+      database: 'lbh3'
+    });
+    client.connect();
+    client.query(`SELECT events.* FROM events_hashers INNER JOIN events ON (events_hashers.trail_number = events.trail_number) WHERE hasher_id=${hasherId} ORDER BY events.start_datetime ${sortDirection} LIMIT 2`, (error, response) => {
+      if (error) {
+        reject(error);
+      } else {
+        resolve(response && response.rows || []);
+      }
+      client.end();
+    });
+  });
+};
 
 const restrictToBored = [
   authenticate('jwt'),
   authHook.restrictToBored()
 ];
+
+const getFirstAndLastTrailData = function({event, hasher}) {
+  return new Promise(function(resolve, reject) {
+    getFirstOrLastTrailForHasher(true, hasher.id).then(firstTrails => {
+      let newFirstTrail;
+      if (firstTrails.length) {
+        newFirstTrail = (firstTrails[0].start_datetime.getTime() === event.startDatetime.getTime()) ? firstTrails[1] : firstTrails[0];
+      }
+      getFirstOrLastTrailForHasher(false, hasher.id).then(lastTrails => {
+        let newLastTrail;
+        if (lastTrails.length) {
+          newLastTrail = (lastTrails[0].start_datetime.getTime() === event.startDatetime.getTime()) ? lastTrails[1] : lastTrails[0];
+        }
+        resolve({
+          newFirstTrail,
+          newLastTrail
+        });
+      }, reject);
+    }, reject);
+  });
+};
 
 const getRequiredData = function(hook) {
   return new Promise(function(resolve, reject) {
@@ -19,6 +59,7 @@ const getRequiredData = function(hook) {
         hook.app.service('api/hashers').get(eventHasher.hasherId).then(hasher => {
           resolve({
             event: events.data[0],
+            eventHasher,
             hasher
           });
         }, reject);
@@ -30,15 +71,34 @@ const getRequiredData = function(hook) {
 const createHook = function(hook) {
   return new Promise(function(resolve, reject) {
     getRequiredData(hook).then(({event, hasher}) => {
+      const didHare = hook.data.role.substr(0, 4) === 'Hare';
       const eventMiles = Number(event.miles);
 
+      let firstTrailDate;
+      let firstTrailNumber;
+      if (!hasher.firstTrailDate || new Date(hasher.firstTrailDate) > event.startDatetime) {
+        // No first trail date or it’s after this event, so use this event’s info
+        firstTrailDate = event.startDatetime;
+        firstTrailNumber = event.trailNumber;
+      } else {
+        // Hasher’s first trail on record is before this event, so use what’s on record
+        firstTrailDate = hasher.firstTrailDate;
+        firstTrailNumber = hasher.firstTrailNumber;
+      }
+
+      const lastTrailDate = (!hasher.lastTrailDate || new Date(hasher.lastTrailDate) < event.startDatetime) ? event.startDatetime : hasher.lastTrailDate;
+
       const hasherPatchData = {
+        firstTrailDate,
+        firstTrailNumber,
+        hareCount1: (didHare) ? (hasher.hareCount1 + 1) : hasher.hareCount1,
+        hareCount2: (didHare) ? (hasher.hareCount2 + 1) : hasher.hareCount2,
+        lastTrailDate,
         miles: Number(hasher.miles) + eventMiles,
         runCount: Number(hasher.runCount) + 1,
         runMileage: Number(hasher.runMileage) + eventMiles
       };
       hook.app.service('api/hashers').patch(hasher.id, hasherPatchData).then(() => {
-
         const eventPatchData = {
           hashersTotal: Number(event.hashersTotal) + 1
         };
@@ -52,21 +112,27 @@ const createHook = function(hook) {
 
 const removeHook = function(hook) {
   return new Promise(function(resolve, reject) {
-    getRequiredData(hook).then(({event, hasher}) => {
-      const eventMiles = Number(event.miles);
-
-      const hasherPatchData = {
-        miles: Number(hasher.miles) - eventMiles,
-        runCount: Number(hasher.runCount) - 1,
-        runMileage: Number(hasher.runMileage) - eventMiles
-      };
-      hook.app.service('api/hashers').patch(hasher.id, hasherPatchData).then(() => {
-
-        const eventPatchData = {
-          hashersTotal: Number(event.hashersTotal) - 1
+    getRequiredData(hook).then(({event, eventHasher, hasher}) => {
+      getFirstAndLastTrailData({event, hasher}).then(({newFirstTrail, newLastTrail}) => {
+        const didHare = eventHasher.role.substr(0, 4) === 'Hare';
+        const eventMiles = Number(event.miles);
+        const hasherPatchData = {
+          firstTrailDate: (newFirstTrail) ? newFirstTrail.start_datetime : null,
+          firstTrailNumber: (newFirstTrail) ? newFirstTrail.trail_number : null,
+          hareCount1: (didHare) ? (hasher.hareCount1 - 1) : hasher.hareCount1,
+          hareCount2: (didHare) ? (hasher.hareCount2 - 1) : hasher.hareCount2,
+          lastTrailDate: (newLastTrail) ? newLastTrail.start_datetime : null,
+          miles: Number(hasher.miles) - eventMiles,
+          runCount: Number(hasher.runCount) - 1,
+          runMileage: Number(hasher.runMileage) - eventMiles
         };
-        hook.app.service('api/events').patch(event.id, eventPatchData).then(() => {
-          resolve(hook);
+        hook.app.service('api/hashers').patch(hasher.id, hasherPatchData).then(() => {
+          const eventPatchData = {
+            hashersTotal: Number(event.hashersTotal) - 1
+          };
+          hook.app.service('api/events').patch(event.id, eventPatchData).then(() => {
+            resolve(hook);
+          }, reject);
         }, reject);
       }, reject);
     }, reject);
