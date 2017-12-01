de8f247f6720ec39c3754cd5ded67fc8a8ce67e1
Chasen Le Hara <chasen@chasenlehara.com>
2017-11-30 23:19:13 -0800

    Add ability to upload hasher profile photos
    Part of https://github.com/LBH3/lbh3.org/issues/81

--- public/components/hasher/hasher.js
+++ public/components/hasher/hasher.js
@@ -5,6 +5,58 @@ import Session from '~/models/session';
 import './hasher.less';
 import view from './hasher.stache';
 
+const uuid = function(a) {
+  // From https://gist.github.com/jed/982883
+  return a?(a^Math.random()*16>>a/4).toString(16):([1e7]+-1e3+-4e3+-8e3+-1e11).replace(/[018]/g,uuid);
+};
+
+const getSignedRequest = function(file, hasher) {
+  return new Promise((resolve, reject) => {
+    const fileExtension = file.name.split('.').pop();
+    if (!fileExtension) {
+      reject('File extension could not be determined by name');
+    } else {
+      const fileName = encodeURIComponent(`${uuid()}.${fileExtension}`);
+      const xhr = new XMLHttpRequest();
+      xhr.open('GET', `/sign-s3?config-bucket-name=headshotsBucketName&file-name=${fileName}&file-type=${file.type}`);
+      xhr.onerror = () => {
+        reject(xhr);
+      };
+      xhr.onreadystatechange = () => {
+        if (xhr.readyState === 4) {
+          if (xhr.status === 200) {
+            const response = JSON.parse(xhr.responseText);
+            resolve(response);
+          } else {
+            reject(xhr);
+          }
+        }
+      };
+      xhr.send();
+    }
+  });
+};
+
+const uploadFile = function(file, signedRequest, url) {
+  return new Promise((resolve, reject) => {
+    const xhr = new XMLHttpRequest();
+    xhr.open('PUT', signedRequest);
+    xhr.onerror = () => {
+      reject(xhr);
+    };
+    xhr.onreadystatechange = () => {
+      if (xhr.readyState === 4) {
+        if (xhr.status === 200) {
+          resolve(true);
+        } else {
+          reject(xhr);
+        }
+      }
+    };
+    xhr.send(file);
+  });
+};
+
 export const ViewModel = DefineMap.extend({
   hasher: {
     get: function(lastValue, setValue) {
@@ -21,6 +73,7 @@ export const ViewModel = DefineMap.extend({
       });
     }
   },
+  headshotPromise: Promise,
   id: 'number',
 
   /**
@@ -36,5 +89,20 @@ export const ViewModel = DefineMap.extend({
 export default Component.extend({
   tag: 'lbh3-hasher',
   ViewModel,
-  view
+  view,
+  events: {
+    'input[type="file"] change': function(input) {
+      const file = (input.files && input.files[0]) ? input.files[0] : null;
+      if (file) {
+        const viewModel = this.viewModel;
+        const hasher = viewModel.hasher;
+        viewModel.headshotPromise = getSignedRequest(file, hasher).then(response => {
+          return uploadFile(file, response.signedRequest, response.url).then(() => {
+            hasher.headshotUrl = response.url;
+            return hasher.save();
+          });
+        });
+      }
+    }
+  }
 });
