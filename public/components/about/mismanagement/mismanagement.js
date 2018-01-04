d405885f1469c5e9c1712523c5b305350f7961e9
Chasen Le Hara <chasen@chasenlehara.com>
2018-01-04 00:02:36 -0800

    Fix some issues on the Mismanagement page
    - Show the last Bored year by default, even after a new calendar year starts
    - Only show the email address link for the current Bored
    - Change the email link to an icon
    - Add links to the Bored members’ profiles if the user has permission

--- public/components/about/mismanagement/mismanagement.js
+++ public/components/about/mismanagement/mismanagement.js
@@ -9,6 +9,18 @@ import '~/components/about/mismanagement/year/';
 const currentYear = (new Date()).getFullYear();
 
 export const ViewModel = DefineMap.extend({
+  mostRecentYear: {
+    get: function(lastValue, setValue) {
+      if (lastValue) {
+        return lastValue;
+      }
+      this.yearsPromise.then(years => {
+        if (years.length > 0) {
+          setValue(years[0].year);
+        }
+      });
+    }
+  },
   routeForYear: function(year) {
     const routeParams = {page: 'about', secondaryPage: 'mismanagement'};
     if (currentYear !== year) {
@@ -27,10 +39,16 @@ export const ViewModel = DefineMap.extend({
       return (filteredYears && filteredYears.length) ? filteredYears[0] : null;
     }
   },
+  showEmailLink: {
+    type: 'boolean',
+    get: function() {
+      return this.mostRecentYear === this.year;
+    }
+  },
   year: {
     type: 'number',
     get: function(year) {
-      return year || currentYear;
+      return year || this.mostRecentYear || currentYear;
     }
   },
   years: {
--- public/components/about/mismanagement/year/year.js
+++ public/components/about/mismanagement/year/year.js
@@ -3,7 +3,9 @@ import BoredYear from '~/models/bored-year';
 import Component from 'can-component';
 import DefineList from 'can-define/list/';
 import DefineMap from 'can-define/map/';
+import Session from '~/models/session';
 import './year.less';
+import route from 'can-route';
 import view from './year.stache';
 
 export const ViewModel = DefineMap.extend({
@@ -46,6 +48,20 @@ export const ViewModel = DefineMap.extend({
         }
       });
     }
+  },
+  routeForHasher: function(hasher) {
+    const routeParams = {
+      id: hasher.id,
+      page: 'hashers'
+    };
+    return route.url(routeParams);
+  },
+  get session() {
+    return Session.current;
+  },
+  showEmailLink: {
+    type: 'boolean',
+    value: false
   },
   year: {
     Type: BoredYear
