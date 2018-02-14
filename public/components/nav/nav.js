7f5d1cd1c77ee3abd5f071ea44445aaf2284af1c
Chasen Le Hara <chasen@chasenlehara.com>
2018-02-13 19:40:02 -0800

    Add Hashers to the main nav

--- public/components/nav/nav.js
+++ public/components/nav/nav.js
@@ -1,10 +1,14 @@
 import Component from 'can-component';
 import DefineMap from 'can-define/map/';
+import Session from '~/models/session';
 import './nav.less';
 import view from './nav.stache';
 
 export const ViewModel = DefineMap.extend({
   page: 'string',
+  get session() {
+    return Session.current;
+  },
   year: 'number'
 });
 
