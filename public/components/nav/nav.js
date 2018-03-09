c3f353fc5d9723957c05f365c8481c7be23ca88b
Chasen Le Hara <chasen@chasenlehara.com>
2018-03-08 18:50:38 -0800

    Highlight the Hareline link in the nav when viewing a future run

--- public/components/nav/nav.js
+++ public/components/nav/nav.js
@@ -5,6 +5,21 @@ import './nav.less';
 import view from './nav.stache';
 
 export const ViewModel = DefineMap.extend({
+  day: 'number',
+  isActive: function(page) {
+    const currentPage = this.page;
+    if (currentPage === 'events' && (page === 'events' || page === 'hareline') && this.year) {
+      const currentDate = new Date();
+      const isFutureYear = this.year >= currentDate.getFullYear();
+      const isFutureMonth = isFutureYear && this.month >= (currentDate.getMonth() + 1);
+      const isFutureDay = isFutureMonth && this.day >= currentDate.getDate();
+      if (isFutureDay) {
+        return page === 'hareline';
+      }
+    }
+    return currentPage === page;
+  },
+  month: 'number',
   page: 'string',
   get session() {
     return Session.current;
