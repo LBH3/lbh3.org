3222e10a978ddd19dae0843cfb3267ea433690a7
Chasen Le Hara <chasen@chasenlehara.com>
2018-03-11 16:44:22 -0700

    Run built production tests in Travis & Sauce Labs

---
+++ public/build-tests.js
@@ -0,0 +1,11 @@
+var stealTools = require("steal-tools");
+
+var buildPromise = stealTools.build({
+  config: __dirname + "/package.json!npm",
+  main: "lbh3/test"
+}, {
+  bundleAssets: {
+    glob: 'images/*',
+    infer: false
+  }
+});
