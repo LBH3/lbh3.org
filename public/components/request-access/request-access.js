b3030a4ce0ec8658cbfae1bce364469aaa864d51
Chasen Le Hara <chasen@chasenlehara.com>
2018-04-10 22:08:37 -0700

    Restrict access to each run’s photos & Snooze
    Also make it easier for hashers who have signed in to request access.
    
    Closes https://github.com/LBH3/lbh3.org/issues/121

---
+++ public/components/request-access/request-access.js
@@ -0,0 +1,31 @@
+import Component from 'can-component';
+import DefineMap from 'can-define/map/';
+import Session from '~/models/session';
+import './request-access.less';
+import view from './request-access.stache';
+
+export const ViewModel = DefineMap.extend({
+  requestedName: {
+    type: 'string'
+  },
+  save: function() {
+    const user = this.session.user;
+    user.requestedName = this.requestedName;
+    this.savingPromise = user.save();
+  },
+  savingPromise: Promise,
+  get session() {
+    return Session.current;
+  }
+});
+
+export default Component.extend({
+  tag: 'lbh3-request-access',
+  ViewModel,
+  view,
+  events: {
+    '{element} submit': function(element, event) {
+      event.preventDefault();
+    }
+  }
+});
---
+++ public/components/request-access/request-access.md
@@ -0,0 +1,11 @@
+@parent lbh3
+@module {can.Component} ~/components/request-access <lbh3-request-access>
+
+A short description of the lbh3-request-access component
+
+@signature `<lbh3-request-access>`
+
+@body
+
+## Use
+
