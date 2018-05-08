2f5f0cb132477bebb0312a1168a85768fd963623
Chasen Le Hara <chasen@chasenlehara.com>
2018-05-07 22:09:45 -0700

    Create a more mobile-friendly layout for the Hareline

--- public/test.js
+++ public/test.js
@@ -30,6 +30,7 @@ import 'lbh3/components/request-access/request-access-test';
 import 'lbh3/components/run/attendance/attendance-test';
 import 'lbh3/components/run/edit/edit-test';
 import 'lbh3/components/run/run-test';
+import 'lbh3/components/run-list/run-list-test';
 import 'lbh3/components/snooze-button/snooze-button-test';
 import 'lbh3/components/special-event/edit/edit-test';
 import 'lbh3/components/special-event/special-event-test';
