88531e3d129a897010bee83a335592e88e72afd9
Chasen Le Hara <chasen@chasenlehara.com>
2018-09-04 23:12:26 +0200

    Add a link on the home page to the memorial service

--- public/components/home/home.js
+++ public/components/home/home.js
@@ -11,8 +11,8 @@ export default Component.extend({
     description: {
       default: 'The Long Beach kennel was founded over 30 years ago. We run Thursday evening during Spring/Summer & Sunday morning in the Fall/Winter. We often have an attendance of 75+. Visitors & Virgins are always welcome! Your run donation covers Hare essentials, pre– and post–run beer/soda/munchies. Please bring a vessel for your beverage. We hope to see you at LBH3. On On!!!'
     },
-    get isBeforeJuly23() {
-      return new Date() < new Date('Jul 23 2018 07:00:00 UTC');
+    get isBeforeOctober13() {
+      return new Date() < new Date('Oct 13 2018 07:00:00 UTC');
     },
     get ogTitle() {
       return this.title;
