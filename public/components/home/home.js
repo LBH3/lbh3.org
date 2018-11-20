b46d40c1a260d0900778226cf7d0ef2e10cc6176
Chasen Le Hara <git@chasenlehara.com>
2018-11-19 23:58:59 -0800

    Show a link to the Found’ er Balls 2019 page
    Also dynamically fetch upcoming elections.

--- public/components/home/home.js
+++ public/components/home/home.js
@@ -1,5 +1,6 @@
 import Component from 'can-component';
 import DefineMap from 'can-define/map/';
+import Election from '~/models/election';
 import Session from '~/models/session';
 import './home.less';
 import view from './home.stache';
@@ -11,14 +12,32 @@ export default Component.extend({
     description: {
       default: 'The Long Beach kennel was founded over 30 years ago. We run Thursday evening during Spring/Summer & Sunday morning in the Fall/Winter. We often have an attendance of 75+. Visitors & Virgins are always welcome! Your run donation covers Hare essentials, pre– and post–run beer/soda/munchies. Please bring a vessel for your beverage. We hope to see you at LBH3. On On!!!'
     },
+    get electionsPromise() {
+      if (this.showUpcomingElections) {
+        return Election.connection.getList({
+          $limit: 100,
+          $sort: {
+            endDatetime: 1
+          }
+        });
+      }
+    },
     get isBeforeJanuary21() {
       return new Date() < new Date('Jan 21 2019 08:00:00 UTC');
+    },
+    get isBeforeFebruary17() {
+      return new Date() < new Date('Feb 17 2019 08:00:00 UTC');
     },
     get ogTitle() {
       return this.title;
     },
     get session() {
       return Session.current;
+    },
+    get showUpcomingElections() {
+      const session = this.session || {};
+      const user = session.user || {};
+      return user.canManageUsers;
     },
     get title() {
       return 'LBH3';
