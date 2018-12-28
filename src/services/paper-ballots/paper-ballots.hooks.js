971192584b3e1fb8054e5cb27a550b331e802db7
Chasen Le Hara <git@chasenlehara.com>
2018-12-27 23:01:37 -0800

    Allow users to fetch their own paper ballots

--- src/services/paper-ballots/paper-ballots.hooks.js
+++ src/services/paper-ballots/paper-ballots.hooks.js
@@ -3,6 +3,11 @@ const authHook = require('../../hooks/auth');
 
 const beforeHook = function(hook) {
 
+  // Allow users to fetch their own paper ballots
+  if (hook.params.query.hasherId && Number(hook.params.query.hasherId) === hook.params.user.hasherId) {
+    return hook;
+  }
+
   // Special exception
   if (hook.params.user.hasherId === 189) {
     return hook;
