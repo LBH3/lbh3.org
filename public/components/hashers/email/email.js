2f58dc9cb6990f7c37dba4800efcc972cb383d89
Chasen Le Hara <git@chasenlehara.com>
2019-09-08 14:44:42 +0100

    Fix a bug where not all emails were being fetched on the Email List page

--- public/components/hashers/email/email.js
+++ public/components/hashers/email/email.js
@@ -5,6 +5,33 @@ import Session from '~/models/session';
 import './email.less';
 import view from './email.stache';
 
+const $limit = 500;
+
+const getHashers = ($skip) => {
+  return Hasher.getList({
+    $or: [
+      {emailing: '1'},
+      {emailing: '2'},
+      {emailing: '3'},
+      {emailing: '4'},
+      {emailing: '5'},
+      {emailing: '5a'},
+      {emailing: 'A'},
+      {emailing: 'B'},
+      {emailing: 'C'},
+      {emailing: 'c'},
+      {emailing: 'R'},
+      {emailing: 'y'},
+      {emailing: 'Yes'}
+    ],
+    $limit,
+    $skip,
+    $sort: {
+      emailing: 1
+    }
+  });
+};
+
 export const ViewModel = DefineMap.extend({
   description: {
     default: ''
@@ -22,26 +49,30 @@ export const ViewModel = DefineMap.extend({
   },
   hashersPromise: {
     get: function() {
-      return Hasher.getList({
-        $or: [
-          {emailing: '1'},
-          {emailing: '2'},
-          {emailing: '3'},
-          {emailing: '4'},
-          {emailing: '5'},
-          {emailing: '5a'},
-          {emailing: 'A'},
-          {emailing: 'B'},
-          {emailing: 'C'},
-          {emailing: 'c'},
-          {emailing: 'R'},
-          {emailing: 'y'},
-          {emailing: 'Yes'}
-        ],
-        $limit: 500,
-        $sort: {
-          emailing: 1
-        }
+      return new Promise((resolve, reject) => {
+        let hashers = [];
+        let result = getHashers(0);
+
+        result.then(initialResults => {
+
+          // Add the first set to the array
+          hashers = [...hashers, ...initialResults];
+
+          const total = initialResults.total;
+          const numberOfPages = Math.ceil(total / $limit);
+
+          for (let i = 1; i < numberOfPages; i++) {
+            result = result.then(() => {
+              return getHashers($limit * i).then(results => {
+                hashers = [...hashers, ...results];
+              }, reject);
+            }, reject);
+          }
+
+          result.then(() => {
+            resolve(hashers);
+          }, reject);
+        }, reject);
       });
     }
   },
