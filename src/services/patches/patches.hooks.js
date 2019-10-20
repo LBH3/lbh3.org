bcd81a3754c887a6cf301521a3705d9735169e05
Chasen Le Hara <git@chasenlehara.com>
2019-10-20 23:22:52 +0100

    Give Grandmasters and Hash Cash access to patch data

--- src/services/patches/patches.hooks.js
+++ src/services/patches/patches.hooks.js
@@ -2,7 +2,15 @@ const { authenticate } = require('@feathersjs/authentication').hooks;
 const authHook = require('../../hooks/auth');
 const errors = require('@feathersjs/errors');
 
-const boredPositions = [authHook.HASH_HISTORIANS, authHook.ON_DISK, authHook.ON_SEC, authHook.TRAILMASTERS, authHook.WEBMASTERS];
+const boredPositions = [
+  authHook.GRANDMASTERS,
+  authHook.HASH_CASH,
+  authHook.HASH_HISTORIANS,
+  authHook.ON_DISK,
+  authHook.ON_SEC,
+  authHook.TRAILMASTERS,
+  authHook.WEBMASTERS
+];
 const restrictToBored = authHook.restrictToUserOrPositions(...boredPositions);
 
 const restrictHook = function(hook) {
