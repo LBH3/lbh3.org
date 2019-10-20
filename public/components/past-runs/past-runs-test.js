e668ad5636abc2a85b75818dc777422e7c9999e5
Chasen Le Hara <git@chasenlehara.com>
2019-10-20 15:34:31 +0100

    Redesign the sign in prompt on the Past Runs page

--- public/components/past-runs/past-runs-test.js
+++ public/components/past-runs/past-runs-test.js
@@ -1,5 +1,4 @@
 import QUnit from 'steal-qunit';
-import { ViewModel } from './past-runs';
 
 // ViewModel unit tests
 QUnit.module('lbh3/components/past-runs');
--- public/components/past-runs/past-runs.js
+++ public/components/past-runs/past-runs.js
@@ -1,5 +1,4 @@
 import Component from 'can-component';
-import DefineMap from 'can-define/map/';
 import Session from '~/models/session';
 import Year from '~/models/year';
 import route from 'can-route';
@@ -10,82 +9,78 @@ import '~/components/year/';
 
 const currentYear = (new Date()).getFullYear();
 
-export const ViewModel = DefineMap.extend({
-  description: {
-    get: function() {
-      return `Archive of LBH3’s runs in ${this.year}.`;
-    }
-  },
-  get ogTitle() {
-    return 'Past Runs';
-  },
-  routeForYear: function(year) {
-    const routeParams = {
-      page: 'events',
-      secondaryPage: '',
-      year
-    };
-    return route.url(routeParams);
-  },
-  get session() {
-    return Session.current;
-  },
-  showHashit: {
-    default: false,
-    type: 'boolean'
-  },
-  showNotes: {
-    default: false,
-    type: 'boolean'
-  },
-  showOnOn: {
-    default: false,
-    type: 'boolean'
-  },
-  showScribe: {
-    default: false,
-    type: 'boolean'
-  },
-  get title() {
-    return `${this.ogTitle} | LBH3`;
-  },
-  year: {
-    type: 'number',
-    get: function(year) {
-      if (year) {
-        return year;
-      }
-      const years = this.years;
-      if (years) {
-        const filtered = years.filter({id: currentYear});
-        if (filtered.length === 0) {
-          return currentYear - 1;
-        }
-      }
-      return currentYear;
-    }
-  },
-  years: {
-    get: function(lastValue, setValue) {
-      if (lastValue) {
-        return lastValue;
-      }
-      this.yearsPromise.then(setValue);
-    }
-  },
-  yearsPromise: {
-    default: () => {
-      return Year.getList({
-        $sort: {
-          id: -1
-        }
-      });
-    }
-  }
-});
-
 export default Component.extend({
   tag: 'lbh3-past-runs',
-  ViewModel,
-  view
+  view,
+  ViewModel: {
+    get description() {
+      return `Archive of LBH3’s runs in ${this.year}.`;
+    },
+    get ogTitle() {
+      return 'Past Runs';
+    },
+    routeForYear: function(year) {
+      const routeParams = {
+        page: 'events',
+        secondaryPage: '',
+        year
+      };
+      return route.url(routeParams);
+    },
+    get session() {
+      return Session.current;
+    },
+    showHashit: {
+      default: false,
+      type: 'boolean'
+    },
+    showNotes: {
+      default: false,
+      type: 'boolean'
+    },
+    showOnOn: {
+      default: false,
+      type: 'boolean'
+    },
+    showScribe: {
+      default: false,
+      type: 'boolean'
+    },
+    get title() {
+      return `${this.ogTitle} | LBH3`;
+    },
+    year: {
+      type: 'number',
+      get: function(year) {
+        if (year) {
+          return year;
+        }
+        const years = this.years;
+        if (years) {
+          const filtered = years.filter({id: currentYear});
+          if (filtered.length === 0) {
+            return currentYear - 1;
+          }
+        }
+        return currentYear;
+      }
+    },
+    years: {
+      get: function(lastValue, setValue) {
+        if (lastValue) {
+          return lastValue;
+        }
+        this.yearsPromise.then(setValue);
+      }
+    },
+    yearsPromise: {
+      default: () => {
+        return Year.getList({
+          $sort: {
+            id: -1
+          }
+        });
+      }
+    }
+  }
 });
