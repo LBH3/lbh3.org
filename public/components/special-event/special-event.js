358883be36c85222ec8b993c1df61703a70643db
Chasen Le Hara <git@chasenlehara.com>
2019-11-16 09:56:54 -0600

    Support dark mode maps on special event pages

--- public/components/special-event/special-event.js
+++ public/components/special-event/special-event.js
@@ -3,6 +3,7 @@ import DefineMap from 'can-define/map/';
 import Session from '~/models/session';
 import SpecialEvent from '~/models/special-event';
 import './special-event.less';
+import stache from 'can-stache';
 import view from './special-event.stache';
 
 export const ViewModel = DefineMap.extend({
@@ -22,26 +23,45 @@ export const ViewModel = DefineMap.extend({
     }
     return 'Special Event';
   },
+  secondaryPage: 'string',
   get session() {
     return Session.current;
   },
-  specialEvent: SpecialEvent,
+  specialEvent: {
+    get(lastSetValue, resolve) {
+      this.specialEventPromise.then(specialEvents => {
+        resolve(specialEvents[0]);
+      });
+    }
+  },
   specialEventPromise: {
     get: function() {
       const params = {
         urlId: this.urlId,
         year: this.year
       };
-      return SpecialEvent.getList(params).then(specialEvents => {
-        this.specialEvent = specialEvents[0];
-      });
+      return SpecialEvent.getList(params);
     }
   },
   get title() {
     return `${this.ogTitle} | LBH3`;
   },
   urlId: 'string',
-  year: 'number'
+  year: 'number',
+
+  connectedCallback(element) {
+    const listener = () => {
+      const session = this.session;
+      const specialEvent = this.specialEvent;
+      const html = session && session.user && session.user.canEditFutureSpecialEvents ? specialEvent.descriptionWithoutTitleHtml : specialEvent.descriptionHtml;
+      element.appendChild(stache(html)());
+    };
+    if (this.specialEvent) {
+      listener();
+    } else {
+      this.listenTo('specialEvent', listener);
+    }
+  }
 });
 
 export default Component.extend({
