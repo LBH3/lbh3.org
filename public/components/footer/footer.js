83ffebc851c0c610204c355f4ba3fa7221de1339
Chasen Le Hara <git@chasenlehara.com>
2020-03-22 16:32:56 +1300

    Make the footer’s border match other borders

--- public/components/footer/footer.js
+++ public/components/footer/footer.js
@@ -4,11 +4,5 @@ import './footer.less';
 
 export default Component.extend({
   tag: 'lbh3-footer',
-  view: `
-    <footer class="text-muted">
-      <p class="my-3 text-center">
-        <a href="mailto:contact@lbh3.org">Email us</a> with any questions or comments.
-      </p>
-    </footer>
-  `
+  view: '<footer class="border-top mt-3 text-muted"><p class="my-3 text-center"><a href="mailto:contact@lbh3.org">Email us</a> with any questions or comments.</p></footer>'
 });
