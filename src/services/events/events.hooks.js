0e5bd17b0d05866c960eaee0a795977c98e4e309
Chasen Le Hara <github@chasenlehara.com>
2020-06-17 20:24:59 -0700

    Add the COVID message to our Google Calendar events
    Also:
    
    - Update the COVID text on the homepage and Hareline
    - Upgrade the googleapis and marked packages

--- src/services/events/events.hooks.js
+++ src/services/events/events.hooks.js
@@ -1,57 +1,60 @@
 /*eslint no-console: ["error", { allow: ["info", "warn", "error"] }] */
-const { authenticate } = require('@feathersjs/authentication').hooks;
-const authHook = require('../../hooks/auth');
-const Entities = require('html-entities').AllHtmlEntities;
-const { google } = require('googleapis');
-const jwtAuthentication = authenticate('jwt');
-const makeRaw = require('../../utils/make-raw');
-const marked = require('marked');
-const striptags = require('striptags');
+const { authenticate } = require("@feathersjs/authentication").hooks;
+const authHook = require("../../hooks/auth");
+const Entities = require("html-entities").AllHtmlEntities;
+const { google } = require("googleapis");
+const jwtAuthentication = authenticate("jwt");
+const makeRaw = require("../../utils/make-raw");
+const marked = require("marked");
+const striptags = require("striptags");
 
 const allowedFields = [
-  'bringMd',
-  'directionsMd',
-  'fromTheHaresMd',
-  'haresMd',
-  'id',
-  'locationGooglePlaceId',
-  'locationMd',
-  'miles',
-  'nameMd',
-  'onOnGooglePlaceId',
-  'onOnMd',
-  'specialEventId',
-  'startDatetime',
-  'trailNumber'
+  "bringMd",
+  "directionsMd",
+  "fromTheHaresMd",
+  "haresMd",
+  "id",
+  "locationGooglePlaceId",
+  "locationMd",
+  "miles",
+  "nameMd",
+  "onOnGooglePlaceId",
+  "onOnMd",
+  "specialEventId",
+  "startDatetime",
+  "trailNumber",
 ];
-const calendar = google.calendar('v3');
-const calendarEmail = 'lbh3-643@lbh3-171321.iam.gserviceaccount.com';
+const calendar = google.calendar("v3");
+const calendarEmail = "lbh3-643@lbh3-171321.iam.gserviceaccount.com";
 const entities = new Entities();
 
 marked.setOptions({
   breaks: true,
-  gfm: true
+  gfm: true,
 });
 
-const attachAuthInfo = function(hook) {
-  return new Promise(function(resolve) {
-    jwtAuthentication(hook).then(() => {
-      resolve(hook);
-    }, () => {
-      resolve(hook);
-    });
+const attachAuthInfo = function (hook) {
+  return new Promise(function (resolve) {
+    jwtAuthentication(hook).then(
+      () => {
+        resolve(hook);
+      },
+      () => {
+        resolve(hook);
+      }
+    );
   });
 };
 
-const authorize = function() {
-  return new Promise(function(resolve, reject) {
+const authorize = function () {
+  return new Promise(function (resolve, reject) {
     const jwtClient = new google.auth.JWT(
       calendarEmail,
       null,
-      process.env.GOOGLE_OAUTH_PRIVATE_KEY.replace(/\\n/g, '\n'),
-      ['https://www.googleapis.com/auth/calendar']
+      process.env.GOOGLE_OAUTH_PRIVATE_KEY.replace(/\\n/g, "\n"),
+      ["https://www.googleapis.com/auth/calendar"]
     );
-    jwtClient.authorize(function(error) {
+    jwtClient.authorize(function (error) {
       if (error) {
         reject(error);
       } else {
@@ -61,38 +64,43 @@ const authorize = function() {
   });
 };
 
-const createEvent = function(auth, resource) {
-  return new Promise(function(resolve, reject) {
-    calendar.events.insert({
-      auth,
-      calendarId: process.env.GOOGLE_CALENDAR_ID,
-      resource
-    }, function(error, response) {
-      if (error) {
-        reject(error);
-      } else {
-        resolve(response);
+const createEvent = function (auth, resource) {
+  return new Promise(function (resolve, reject) {
+    calendar.events.insert(
+      {
+        auth,
+        calendarId: process.env.GOOGLE_CALENDAR_ID,
+        resource,
+      },
+      function (error, response) {
+        if (error) {
+          reject(error);
+        } else {
+          resolve(response);
+        }
       }
-    });
+    );
   });
 };
 
-const filterData = function(data) {
-  if (data && data.data) {// Array of data
-    data.data = data.data.map(record => {
+const filterData = function (data) {
+  if (data && data.data) {
+    // Array of data
+    data.data = data.data.map((record) => {
       return filterData(record);
     });
     return data;
-  } else {// Single object
+  } else {
+    // Single object
     const filteredFields = {};
-    allowedFields.forEach(field => {
+    allowedFields.forEach((field) => {
       filteredFields[field] = data[field];
     });
     return filteredFields;
   }
 };
 
-const filterFields = function(hook) {
+const filterFields = function (hook) {
   // Check if it’s an external call
   if (hook.params.provider) {
     const user = hook.params.user;
@@ -102,8 +110,8 @@ const filterFields = function(hook) {
   }
 };
 
-const getEvent = function(auth, trailData) {
-  return new Promise(function(resolve, reject) {
+const getEvent = function (auth, trailData) {
+  return new Promise(function (resolve, reject) {
     const timeMin = trailData.startDatetime;
     const timeMaxDate = new Date(timeMin);
     timeMaxDate.setMinutes(timeMaxDate.getMinutes() + 15);
@@ -113,149 +121,191 @@ const getEvent = function(auth, trailData) {
       auth,
       calendarId: process.env.GOOGLE_CALENDAR_ID,
       maxResults: 1,
-      orderBy: 'startTime',
+      orderBy: "startTime",
       singleEvents: true,
       timeMax,
-      timeMin
+      timeMin,
     };
-    console.info('Making calendar.events.list call with params:', params);
+    console.info("Making calendar.events.list call with params:", params);
 
-    calendar.events.list(params, function(error, response) {
-      console.info('calendar.events.list callback received arguments:', error, response);
+    calendar.events.list(params, function (error, response) {
+      console.info(
+        "calendar.events.list callback received arguments:",
+        error,
+        response
+      );
       if (error) {
         reject(error);
       } else {
-        const matchingEvent = (response && response.data && response.data.items) ? response.data.items[0] : null;
-        console.info('Found matching event:', matchingEvent);
+        const matchingEvent =
+          response && response.data && response.data.items
+            ? response.data.items[0]
+            : null;
+        console.info("Found matching event:", matchingEvent);
         resolve(matchingEvent);
       }
     });
   });
 };
 
-const getResourceForTrail = function(app, trailData) {
-  return new Promise(function(resolve, reject) {
-    const bringText = markdownToPlain(trailData.bringMd);
-    const directionsText = markdownToPlain(trailData.directionsMd);
-    const fromTheHaresText = markdownToPlain(trailData.fromTheHaresMd);
+const getResourceForTrail = function (app, trailData) {
+  return new Promise(function (resolve, reject) {
+    const bringInline = markdownToInline(trailData.bringMd);
+    const directionsInline = markdownToInline(trailData.directionsMd);
+    const fromTheHaresInline = markdownToInline(trailData.fromTheHaresMd);
+    const haresInline = markdownToInline(trailData.haresMd);
     const haresText = markdownToPlain(trailData.haresMd);
     const location = markdownToPlain(trailData.locationMd);
     const nameText = markdownToPlain(trailData.nameMd);
-    const onOnText = markdownToPlain(trailData.onOnMd);
+    const onOnInline = markdownToInline(trailData.onOnMd);
     const trailNumber = trailData.trailNumber;
 
     // Description
     const descriptionParts = [];
-    if (nameText && haresText) {// If there’s a name for the trail, then we need to show the hares
-      descriptionParts.push(`Hares: ${haresText}`);
+    if (!haresInline) {
+      descriptionParts.push(
+        "<a href='mailto:trailmasters@lbh3.org'>Email our Trail Masters</a> to hare this run."
+      );
     }
-    if (fromTheHaresText) {
-      descriptionParts.push(fromTheHaresText);
+    descriptionParts.push(`LBH3 runs have tentatively restarted. Here’s how hashing with LBH3 will work until further notice:
+
+- <strong>Masks are mandatory</strong> while not running. Bring your own face covering. <strong>If you do not have a mask, you will be asked to leave.</strong>
+- Please pay Hash Cash with exact change ($5), PayPal, or Venmo.
+- Non-keg beer will be made available before, during, and after trail.
+- Limited individually-packaged munchies will be made available after trail.
+- A quick circle will be held after trail. Please feel free to leave if you’re uncomfortable with circle.
+- Maintain 6′ distance from others as much as possible and respect everyone’s boundaries.
+
+If you’re feeling any <a href="https://www.cdc.gov/coronavirus/2019-ncov/symptoms-testing/symptoms.html">symptoms</a>, <strong>please do not come to the hash.</strong>`);
+    if (haresInline && nameText) {
+      // If there’s a name for the trail, then we need to show the hares
+      descriptionParts.push(`Hares: ${haresInline}`);
     }
-    if (haresText) {
+    if (fromTheHaresInline) {
+      descriptionParts.push(fromTheHaresInline);
+    }
+    if (haresInline) {
       if (!trailData.specialEventId) {
-        descriptionParts.push('Donation: $5 for the run');
+        descriptionParts.push("Donation: $5 for the run");
       }
-    } else {
-      descriptionParts.push('Email our Trailmasters to hare this run: trailmasters@lbh3.org');
     }
-    if (bringText) {
-      descriptionParts.push(`Bring: ${bringText}`);
+    if (bringInline) {
+      descriptionParts.push(`Bring: ${bringInline}`);
     }
-    if (directionsText) {
-      descriptionParts.push(`Directions: ${directionsText}`);
+    if (directionsInline) {
+      descriptionParts.push(`Directions: ${directionsInline}`);
     }
-    if (onOnText) {
-      descriptionParts.push(`On on: ${onOnText}`);
+    if (onOnInline) {
+      descriptionParts.push(`On on: ${onOnInline}`);
     }
-    const description = descriptionParts.join('\n\n');
+    const description = descriptionParts.join("\n\n");
 
     // End
     const endDate = new Date(trailData.startDatetime);
     endDate.setHours(endDate.getHours() + 3);
 
     // Summary
-    const summary = 'LBH3—' + (nameText || haresText || `Run #${trailNumber}`);
+    const summary = "LBH3—" + (nameText || haresText || `Run #${trailNumber}`);
 
     // Source
     const startDate = new Date(trailData.startDatetime);
     const year = startDate.getFullYear();
-    const month = ('0' + (startDate.getMonth() + 1)).slice(-2);
-    const day = ('0' + startDate.getDate()).slice(-2);
+    const month = ("0" + (startDate.getMonth() + 1)).slice(-2);
+    const day = ("0" + startDate.getDate()).slice(-2);
     const source = {
       title: summary,
-      url: `https://www.lbh3.org/events/${year}/${month}/${day}/trail-${trailNumber}/`
+      url: `https://www.lbh3.org/events/${year}/${month}/${day}/trail-${trailNumber}/`,
     };
 
     const resource = {
       description,
       end: {
-        dateTime: endDate.toISOString()
+        dateTime: endDate.toISOString(),
       },
       location,
       source,
       start: {
-        dateTime: trailData.startDatetime
+        dateTime: trailData.startDatetime,
       },
-      summary
+      summary,
     };
 
     // Location
     if (trailData.locationGooglePlaceId) {
-      const placeService = app.service('api/places');
-      placeService.find({query: {id: trailData.locationGooglePlaceId}}).then(foundPlaces => {
-        if (foundPlaces && foundPlaces.total > 0) {
-          const foundPlace = foundPlaces.data[0] || {};
-          const address = foundPlace.formattedAddress || resource.location;
-          resource.location = (foundPlace.name && address.indexOf(foundPlace.name) === -1) ? `${foundPlace.name}, ${address}` : address;
-        }
-        resolve(resource);
-      }, reject);
+      const placeService = app.service("api/places");
+      placeService
+        .find({ query: { id: trailData.locationGooglePlaceId } })
+        .then((foundPlaces) => {
+          if (foundPlaces && foundPlaces.total > 0) {
+            const foundPlace = foundPlaces.data[0] || {};
+            const address = foundPlace.formattedAddress || resource.location;
+            resource.location =
+              foundPlace.name && address.indexOf(foundPlace.name) === -1
+                ? `${foundPlace.name}, ${address}`
+                : address;
+          }
+          resolve(resource);
+        }, reject);
     } else {
       resolve(resource);
     }
   });
 };
 
-const markdownToPlain = function(text) {
-  return entities.decode(striptags(marked(text || ''))).trim();
+const markdownToInline = function (text) {
+  return entities
+    .decode(striptags(marked(text || ""), ["a", "del", "em", "strong"]))
+    .trim();
 };
 
-const updateEvent = function(auth, eventId, resource) {
-  return new Promise(function(resolve, reject) {
-    calendar.events.update({
-      auth,
-      calendarId: process.env.GOOGLE_CALENDAR_ID,
-      eventId,
-      resource
-    }, function(error, response) {
-      if (error) {
-        reject(error);
-      } else {
-        resolve(response);
+const markdownToPlain = function (text) {
+  return entities.decode(striptags(marked(text || ""))).trim();
+};
+
+const updateEvent = function (auth, eventId, resource) {
+  return new Promise(function (resolve, reject) {
+    calendar.events.update(
+      {
+        auth,
+        calendarId: process.env.GOOGLE_CALENDAR_ID,
+        eventId,
+        resource,
+      },
+      function (error, response) {
+        if (error) {
+          reject(error);
+        } else {
+          resolve(response);
+        }
       }
-    });
+    );
   });
 };
 
-const syncWithGoogleCalendar = function(hook) {
+const syncWithGoogleCalendar = function (hook) {
   const trailData = hook.data;
 
   if (trailData) {
     // Calculate the datetime that’s 3 hours after the start time
     // 3 hours matches public/models/event.js
     const aLittleWhileAfterTrailStart = new Date(trailData.startDatetime);
-    aLittleWhileAfterTrailStart.setHours(aLittleWhileAfterTrailStart.getHours() + 3);
+    aLittleWhileAfterTrailStart.setHours(
+      aLittleWhileAfterTrailStart.getHours() + 3
+    );
 
     // Only update Google Calendar if the trail is in the future
     if (aLittleWhileAfterTrailStart > new Date()) {
-      return authorize().then(function(auth) {
-        return getEvent(auth, trailData).then(function(matchingEvent) {
-          return getResourceForTrail(hook.app, trailData).then(function(resource) {
-            return (matchingEvent) ? updateEvent(auth, matchingEvent.id, resource) : createEvent(auth, resource);
-          }).then(function() {
-            return hook;
-          });
+      return authorize().then(function (auth) {
+        return getEvent(auth, trailData).then(function (matchingEvent) {
+          return getResourceForTrail(hook.app, trailData)
+            .then(function (resource) {
+              return matchingEvent
+                ? updateEvent(auth, matchingEvent.id, resource)
+                : createEvent(auth, resource);
+            })
+            .then(function () {
+              return hook;
+            });
         });
       });
     }
@@ -265,26 +315,33 @@ const syncWithGoogleCalendar = function(hook) {
 module.exports = {
   before: {
     all: [],
-    find: [ attachAuthInfo ],
-    get: [ attachAuthInfo ],
-    create: [ authenticate('jwt'), authHook.restrictTo(authHook.WEBMASTERS) ],
+    find: [attachAuthInfo],
+    get: [attachAuthInfo],
+    create: [authenticate("jwt"), authHook.restrictTo(authHook.WEBMASTERS)],
     update: [
-      authenticate('jwt'),
-      authHook.restrictTo(authHook.HASH_FLASH, authHook.HASH_HISTORIANS, authHook.ON_DISK, authHook.ON_SEC, authHook.TRAILMASTERS, authHook.WEBMASTERS),
-      makeRaw
+      authenticate("jwt"),
+      authHook.restrictTo(
+        authHook.HASH_FLASH,
+        authHook.HASH_HISTORIANS,
+        authHook.ON_DISK,
+        authHook.ON_SEC,
+        authHook.TRAILMASTERS,
+        authHook.WEBMASTERS
+      ),
+      makeRaw,
     ],
-    patch: [ authenticate('jwt'), authHook.restrictTo() ],
-    remove: [ authenticate('jwt'), authHook.restrictTo(authHook.WEBMASTERS) ]
+    patch: [authenticate("jwt"), authHook.restrictTo()],
+    remove: [authenticate("jwt"), authHook.restrictTo(authHook.WEBMASTERS)],
   },
 
   after: {
     all: [],
-    find: [ filterFields ],
-    get: [ filterFields ],
+    find: [filterFields],
+    get: [filterFields],
     create: [syncWithGoogleCalendar],
     update: [syncWithGoogleCalendar],
     patch: [],
-    remove: []
+    remove: [],
   },
 
   error: {
@@ -294,6 +351,6 @@ module.exports = {
     create: [],
     update: [],
     patch: [],
-    remove: []
-  }
+    remove: [],
+  },
 };
