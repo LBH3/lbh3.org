ad6ed6df3d4ccb0c523f617f276b756e7d04043f
Chasen Le Hara <chasen@chasenlehara.com>
2020-06-30 21:46:46 -0700

    Remove the mention on munchies from the Home and About pages

--- public/components/about/about.js
+++ public/components/about/about.js
@@ -5,7 +5,7 @@ import view from './about.stache';
 
 export const ViewModel = DefineMap.extend({
   description: {
-    default: 'The Long Beach kennel was founded over 30 years ago on January 6th, 1985 by Dal “Jock” Trader, Jerry “Eject” Templeman, and Andy “Zapata” Limon. We run Thursday evening during Spring/Summer & Sunday morning in the Fall/Winter. We often have an attendance of 75+. Visitors & Virgins are always welcome! Your run donation covers Hare essentials, pre– and post–run beer/soda/munchies. Please bring a vessel for your beverage. We hope to see you at LBH3. On On!!!'
+    default: 'The Long Beach kennel was founded over 30 years ago on January 6th, 1985 by Dal “Jock” Trader, Jerry “Eject” Templeman, and Andy “Zapata” Limon. We run Thursday evening during Spring/Summer & Sunday morning in the Fall/Winter. We often have an attendance of 75+. Visitors & Virgins are always welcome! Your run donation covers Hare essentials, pre– and post–run beer. Please bring a vessel for your beverage. We hope to see you at LBH3. On On!!!'
   },
 
   get ogTitle() {
