403a2ac084c420612e058812d858af823e208394
Chasen Le Hara <webmaster@lbh3.org>
2020-12-15 22:39:38 -0800

    Prevent future runs from showing up on the Past Runs page

--- public/components/past-runs/past-runs-test.js
+++ public/components/past-runs/past-runs-test.js
@@ -10,7 +10,7 @@ QUnit.test('eventQuery for current year', function(assert) {
     year
   });
   assert.equal(vm.eventQuery.startDatetime.$gte.getTime(), new Date(`Jan 1 ${year} 08:00:00 UTC`).getTime(), '$gte is correct');
-  assert.equal(vm.eventQuery.startDatetime.$lte.getTime(), vm.currentTime.getTime(), '$lte is correct');
+  assert.equal(vm.eventQuery.startDatetime.$lte.getTime(), new Date(2020, 11, 12).getTime(), '$lte is correct');
 });
 
 QUnit.test('eventQuery for past year', function(assert) {
