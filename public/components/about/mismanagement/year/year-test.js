fc52f7d99484a64257690751934b8c50ca2cbce8
Chasen Le Hara <webmaster@lbh3.org>
2021-04-18 08:11:53 -0700

    Change the “starting in” text to “started in” on the Mismanagement page

--- public/components/about/mismanagement/year/year-test.js
+++ public/components/about/mismanagement/year/year-test.js
@@ -6,7 +6,7 @@ import { ViewModel } from './year';
 // ViewModel unit tests
 QUnit.module('lbh3/components/about/mismanagement/year');
 
-QUnit.test('addendum returns “starting in” text', function(assert) {
+QUnit.test('addendum returns “started in” text', function(assert) {
   const hasher = new Hasher({
     endDate: '2020-02-16',
     startDate: '2019-04-30'
@@ -16,7 +16,7 @@ QUnit.test('addendum returns “starting in” text', function(assert) {
     startDate: '2019-02-17'
   });
   const addendum = new ViewModel().addendum(year, hasher);
-  assert.equal(addendum, 'starting in April');
+  assert.equal(addendum, 'started in April');
 });
 
 QUnit.test('addendum returns “until” text', function(assert) {
