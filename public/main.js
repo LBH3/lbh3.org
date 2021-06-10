7b627970b530af56b145a032495cf1796e8878c8
Chasen Le Hara <webmaster@lbh3.org>
2021-06-09 21:27:43 -0700

    Remove done-autorender

---
+++ public/main.js
@@ -0,0 +1,9 @@
+import Component from 'can-component';
+import view from './main.stache';
+import ViewModel from './app.js';
+
+export default Component.extend({
+  tag: 'lbh3-main',
+  view,
+  ViewModel
+});
