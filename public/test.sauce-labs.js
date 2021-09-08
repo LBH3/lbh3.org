83470bc3b1aac018124f5b8978fdb11352867a54
Chasen Le Hara <webmaster@lbh3.org>
2021-09-07 20:49:56 -0700

    Make sure SauceLabs runs after the server has been started

--- public/test.sauce-labs.js
+++ public/test.sauce-labs.js
@@ -8,52 +8,52 @@ try {
     },
     root: __dirname
   });
-  server.listen(3000);
+  server.listen(3000, function() {
+    // https://github.com/SeleniumHQ/selenium/wiki/DesiredCapabilities
+    testSauceLabs({
+      platforms: [{
+        'appium-version': '1.9.1',
+        browserName: 'Chrome',
+        commandTimeout: 60,
+        deviceName: 'Android Emulator',
+        platform: 'Android',
+        version: '6.0'
+      }, {
+        'appium-version': '1.9.1',
+        browserName: 'Safari',
+        commandTimeout: 60,
+        deviceName: 'iPhone 7 Simulator',
+        platform: 'iOS',
+        version: '10.3'
+      }, {
+        browserName: 'MicrosoftEdge',
+        commandTimeout: 60,
+        platform: 'Windows 10',
+        version: '18'
+      }, {
+        browserName: 'firefox',
+        commandTimeout: 60,
+        platform: 'Windows 7',
+        version: '47'
+      }, {
+        browserName: 'googlechrome',
+        commandTimeout: 60,
+        platform: 'Windows 7',
+        version: '46'
+      }, {
+        browserName: 'safari',
+        commandTimeout: 60,
+        platform: 'OS X 10.12',
+        version: '10'
+      }],
+      runInParallel: false,
+      urls: [{
+        name: 'lbh3-webpack',
+        url: 'http://127.0.0.1:3000/test.production.html?hidepassed'
+      }],
+      zeroAssertionsPass: false
+    });
+  });
 } catch (error) {
   console.error('Error starting server:', error);
 }
-
-// https://github.com/SeleniumHQ/selenium/wiki/DesiredCapabilities
-
-testSauceLabs({
-  platforms: [{
-    'appium-version': '1.9.1',
-    browserName: 'Chrome',
-    commandTimeout: 60,
-    deviceName: 'Android Emulator',
-    platform: 'Android',
-    version: '6.0'
-  }, {
-    'appium-version': '1.9.1',
-    browserName: 'Safari',
-    commandTimeout: 60,
-    deviceName: 'iPhone 7 Simulator',
-    platform: 'iOS',
-    version: '10.3'
-  }, {
-    browserName: 'MicrosoftEdge',
-    commandTimeout: 60,
-    platform: 'Windows 10',
-    version: '18'
-  }, {
-    browserName: 'firefox',
-    commandTimeout: 60,
-    platform: 'Windows 7',
-    version: '47'
-  }, {
-    browserName: 'googlechrome',
-    commandTimeout: 60,
-    platform: 'Windows 7',
-    version: '46'
-  }, {
-    browserName: 'safari',
-    commandTimeout: 60,
-    platform: 'OS X 10.12',
-    version: '10'
-  }],
-  urls: [{
-    name: 'lbh3-webpack',
-    url: 'http://127.0.0.1:3000/test.production.html?hidepassed'
-  }],
-  zeroAssertionsPass: false
-});
