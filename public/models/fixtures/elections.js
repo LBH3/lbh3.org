ef4ee41666c36db04a864e95bbe1f046da594d03
Chasen Le Hara <webmaster@lbh3.org>
2023-01-11 14:30:48 -0800

    Add a fixture for the 2022 election

--- public/models/fixtures/elections.js
+++ public/models/fixtures/elections.js
@@ -990,6 +990,208 @@ You can submit this form until February 13<sup>th</sup>. Cum to [Found ’er Bal
         ]
       }
     }
+  },
+  {
+    id: 6,
+    createdAt: '2022-12-27T15:22:19.890Z',
+    updatedAt: '2022-12-27T15:22:19.890Z',
+    advertise: true,
+    advertisementMd: 'It’s erection time! Vote for members of the 2023 Bored. 🗳',
+    descriptionMd: `#### Instructions
+
+- Vote for the number of positions as noted in each category.
+- Select the checkbox or radio button next to your choice.
+- You don’t have to vote in every race.
+- <a href="https://www.lbh3.org/events/2022/?showHashit=true&showNotes=true&showOnOn=true&showScribe=true" target="_blank">Use the Past Runs page to write in your nominations for the Best and Worst of 2022.</a>
+
+##### Write-in candidates
+
+- To add a candidate, use the search text box to find your candidate, then select their name.
+- Once their name appears next to the other candidates, select the checkbox next to their name (if it is not already checked).
+- Do not write-in a candidate whose name already appears on the ballot.
+
+You can submit this form until January 29<sup>th</sup> at 6 PM. Cum to [Found ’er Balls on February 4<sup>th</sup>](https://www.lbh3.org/events/2023/founders/) to find out the results of the Bored Erection and to celebrate the ups & downs of 2022!`,
+    endDatetime: tomorrow,
+    endedNoticeMd: 'This erection ended on January 29<sup>th</sup>. Cum to [Found ’er Balls on February 4<sup>th</sup>](https://www.lbh3.org/events/2023/founders/) to find out the results and to celebrate the ups & downs of 2022!',
+    publicKey: `
+      -----BEGIN PUBLIC KEY-----
+      MIICIjANBgkqhkiG9w0BAQEFAAOCAg8AMIICCgKCAgEAzs4jXfo7MUy15xt1oPDp
+      J+x9EF39rPwUiKthzPQfs+1oe/k9ccztKQo+wLvTE0VvQzsSk2qk2fklSpyASLCV
+      Mu3u2WbfblwLQItS+lOeKtH9ylIDUpASX0VaBK95VXtxELnbMvoAkCNmo9E/uMZf
+      NP+IPQULyt0D9lumrw4yl3vssetA3gmf2x5FuSdClj5wsVVbQojH8M054Agt3L80
+      AlIV5SAOrGypn8LswaZIgLc6BtnS8bPXYSSriFDF7Z8FSXga5wAHyEcXjBzrYa04
+      wGoDlPhUeBf8LVx+qUwefIbgeSt29MMaLvS7u6drkwLOwm1opsfYZcnxA30qmPhI
+      gO2UvZWMsVJLyshgx1OVg2QJ02UgKA0Erw8cUY68iBVDm5nPNYsqJx3QS53m7W7M
+      HWEZ7fN0raTWWOX+CgrzxQgreOd9L+JDmw/LkYV0uSUNXCYw8HXzbp6nZu9gjobf
+      7nINxD6WwOkttCKRljTrftPkQvBOt8VmlAG/pILF4Z1b6vteuUP0rSb4RRjI/XdS
+      wv2rqKEV4QyaO7bTAZmPl4dVvALkJ22OouuL2xQ0rWiizN32XHZaor5cUlrpbJUP
+      Y7ugiaEg+ikseA71xDSbuZSb94KoAlr2E6bkIvuZpEd9MDP2xG1HQ73h+CUtPe1o
+      9+oFFyfxSvLfXDtawwYqXHECAwEAAQ==
+      -----END PUBLIC KEY-----
+    `,
+    startDatetime: '2022-12-27T15:22:19.890Z',
+    titleMd: '2022&nbsp;Awards & 2023&nbsp;Bored&nbsp;Positions',
+    urlId: '2022',
+    year: 2022,
+    schema: {
+      awards: {
+        title: 'Awards for 2022',
+        races: [
+          {
+            id: 'best-trail',
+            title: 'Best Trail (Run) of 2022',
+            type: 'run'
+          },
+          {
+            id: 'worst-trail',
+            title: 'Worst Trail (Run) of 2022',
+            type: 'run'
+          },
+          {
+            id: 'best-on-on',
+            title: 'Best On-On of 2022',
+            type: 'on-on'
+          },
+          {
+            id: 'most-deserved-hashit',
+            title: 'Most Deserved Hashit of 2022',
+            type: 'hashit'
+          },
+          {
+            id: 'biggest-whiner',
+            title: 'Biggest Whiner of 2022',
+            type: 'hasher'
+          },
+          {
+            id: 'best-scribe',
+            title: 'Best Scribe Write-Up of 2022',
+            type: 'scribe'
+          },
+          {
+            description: 'Select one',
+            id: 'best-rookie',
+            options: [
+              { id: 8576, name: 'Bone Jo Vee', runs: '15' },
+              { id: 8380, name: 'Dick n’ Dots', runs: '15' },
+              { id: 8537, name: 'Fill My Pee, Na', runs: '16' },
+              { id: 7875, name: 'Grandmas Cookie', runs: '19' },
+              { id: 8445, name: 'Therapussy', runs: '18 + 1 Hare' },
+              { id: 8550, name: 'Wiener Juice', runs: '16 + 1 Hare' },
+              { id: 8366, name: 'Your Kid’s a Dick', runs: '22' }
+            ],
+            title: 'Best Rookie of 2022',
+            type: 'hasher'
+          },
+          {
+            description: 'Up to the GMs’ discretion—so be clever and fun',
+            id: 'write-in-awards',
+            title: 'Write-in Awards',
+            type: 'textarea'
+          }
+        ]
+      },
+      positions: {
+        title: '2023 Mismanagement Positions',
+        races: [
+          {
+            id: 'brewmeister',
+            maxSelection: 2,
+            options: [
+              { id: 14, name: 'Last Train To Cuntsville' },
+              { id: 5451, name: 'You’re the Dung That I Want' }
+            ],
+            title: 'Brewmeister'
+          },
+          {
+            id: 'grandmaster',
+            maxSelection: 2,
+            options: [
+              { id: 8296, name: 'Salt Lick [Female]' },
+              { id: 6535, name: 'Transcuntinental' },
+              { id: 7303, name: 'Yoko Anal' }
+            ],
+            title: 'Grandmaster'
+          },
+          {
+            id: 'hash-flash',
+            maxSelection: 2,
+            options: [
+              { id: 18, name: 'Passing Wind' }
+            ],
+            title: 'Hash Flash'
+          },
+          {
+            id: 'munchmeister',
+            maxSelection: 2,
+            options: [
+              { id: 505, name: 'Baby Crack Whore' },
+              { id: 134, name: 'Just Jam It' }
+            ],
+            title: 'Munchmeister'
+          },
+          {
+            id: 'on-sec',
+            maxSelection: 2,
+            options: [
+              { id: 7931, name: 'Don’t A Salt Me' },
+              { id: 301, name: 'Snatch of the Day' }
+            ],
+            title: 'On Sec'
+          },
+          {
+            id: 'haberdasher',
+            maxSelection: 2,
+            options: [
+              { id: 7736, name: 'Oedipus Crab' },
+            ],
+            title: 'Haberdasher'
+          },
+          {
+            id: 'hash-cash',
+            maxSelection: 2,
+            options: [
+              { id: 81, name: 'Achey Breaky Fart' },
+              { id: 25, name: 'Hi-Speed Copulator' }
+            ],
+            title: 'Hash Cash'
+          },
+          {
+            id: 'on-disc',
+            maxSelection: 2,
+            options: [
+              { id: 6143, name: 'Mounds of Joy' },
+              { id: 7559, name: 'Spitty Spitty Gangbang' }
+            ],
+            title: 'On Disc'
+          },
+          {
+            id: 'recyclemeister',
+            maxSelection: 2,
+            options: [
+              { id: 269, name: 'Necrofishiac' }
+            ],
+            title: 'Recyclemeister'
+          },
+          {
+            id: 'trailmaster',
+            maxSelection: 2,
+            options: [
+              { id: 178, name: 'Bust Her Hymen' },
+              { id: 67, name: 'Pillsbury Blow Boy' },
+            ],
+            title: 'Trailmaster'
+          },
+          {
+            id: 'webmeister',
+            maxSelection: 1,
+            options: [
+              { id: 6394, name: 'I’m Fucking Matt Damon' }
+            ],
+            title: 'Webmeister'
+          }
+        ]
+      }
+    }
   }
 ];
 
